55bc45db25fe0598ac855ede26d508bd3f873aae
Debolina Das <debolinadas0404@gmail.com>
2026-03-13 21:56:09 +0530

    Update poly A tail check for eukaryotic mRNA

--- eukmrna.py
+++ eukmrna.py
@@ -1,2 +1,2 @@
 stra = input("Enter your mRNA Transcript sequence: ").strip().upper() #Paste your transcript in a single line
-print("Is your string an eukaryotic sequence: ",stra.endswith("AAA")) #This looks for poly A tail as a signal to recognize eukaryotic mRNA
+print("Is your string an eukaryotic sequence: ",stra.endswith("AAAAAAAAA")) #This looks for poly A tail as a signal to recognize eukaryotic mRNA
